b77b5e6f6042a985b615ca699a418eee4cdfbe5e
arcadia-ego <arcadia-ego@github.com>
2018-07-02 08:54:58 -0700

    backend init

---
+++ User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    }
+});
+
+userSchema.pre("save", function(next) {
+    bcrypt.hash(this.password, 10).then(hash => {
+        this.password = hash;
+        next();
+    });
+});
+
+userSchema.methods.verifyPassword = function(guess, callback){
+    bcrypt.compare(guess, this.password, function(err, isValid){
+        if(err){
+            return callback(err);
+        }
+        callback(null, isValid);
+    });
+}
+
+module.exports = mongoose.model("User", userSchema);
